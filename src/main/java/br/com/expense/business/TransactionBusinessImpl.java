e17c7dddadcab85bec22055f8e841d1e07df945d
Bjornn Borg <bjornnborg@gmail.com>
2013-01-29 23:16:11 -0200

    refactoring to use report generators

--- src/main/java/br/com/expense/business/TransactionBusinessImpl.java
+++ src/main/java/br/com/expense/business/TransactionBusinessImpl.java
@@ -4,15 +4,8 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.text.DateFormat;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 import br.com.expense.config.Configuration;
 import br.com.expense.model.Transaction;
@@ -20,11 +13,10 @@ import br.com.expense.parser.TransactionParser;
 import br.com.expense.parser.TransactionParserEngine;
 import br.com.expense.parser.rules.CategoryRulesEngine;
 import br.com.expense.parser.rules.CategoryRulesParser;
+import br.com.expense.report.CategoryExpensesReport;
+import br.com.expense.report.TransactionRecordReport;
 
 public class TransactionBusinessImpl implements TransactionBusiness {
-	
-	private static final DateFormat DATE_FORMAT = new SimpleDateFormat("dd/MM/yyyy");
-	private static final NumberFormat NUMBER_FORMAT = new DecimalFormat("###,###,##0.00");
 	
 	private TransactionParserEngine parserEngine;
 
@@ -50,39 +42,22 @@ public class TransactionBusinessImpl implements TransactionBusiness {
 		
 		List<Transaction> transactions = this.parserEngine.getTransactions(file);
 		
-		Map<String, BigDecimal> costsByCategory = new HashMap<String, BigDecimal>();
+		StringBuilder reportContent = new StringBuilder();
 		
-		StringBuilder expensesOutput = new StringBuilder();
-		StringBuilder expensesByCategory = new StringBuilder();
+		CategoryExpensesReport categoryExpenses = new CategoryExpensesReport(transactions);
+		reportContent.append(categoryExpenses.getContent());
 		
-		for (Transaction transaction : transactions) {
-			// write file
-			String line = DATE_FORMAT.format(transaction.getDate()) + ";" + transaction.getDescription() + ";" + transaction.getType() + ";" + NUMBER_FORMAT.format(transaction.getCurrencyInfo().getTotalValue());
-			if (transaction.getCategory() != null) {
-				line += ";" + transaction.getCategory().getName();
-			}
-			expensesOutput.append(line + "\r\n");
-			
-			String category = transaction.getCategory() != null ? transaction.getCategory().getName() : "unspecified";
-			// just in case we have same category name for debit or credit
-			category += "^" + transaction.getType();
-			
-			BigDecimal categorySum = costsByCategory.get(category);
-			if (categorySum == null) {
-				categorySum = new BigDecimal(0);
-			}
-			costsByCategory.put(category, categorySum.add(transaction.getCurrencyInfo().getTotalValue()));			
-		}
+		reportContent.append("\r\n");
+		reportContent.append("\r\n");
 		
-		for (String category : costsByCategory.keySet()) {
-			expensesByCategory.append(category.split("\\^")[0] + ";" + NUMBER_FORMAT.format(costsByCategory.get(category)) + "\r\n");
-		}		
+		TransactionRecordReport transactionRecords = new TransactionRecordReport(transactions);
+		reportContent.append(transactionRecords.getContent());
+		
 			
 		BufferedWriter bw = null;
 		try {
 			 bw = new BufferedWriter(new FileWriter("expenses-report.csv"));
-			 bw.write(expensesByCategory.toString() + "\r\n\r\n" + expensesOutput.toString()+"\r\n");
-			 bw.write("=soma(d1:d50)");
+			 bw.write(reportContent.toString());
 			 bw.close();
 		} catch (IOException e) {
 			if (bw != null) {
