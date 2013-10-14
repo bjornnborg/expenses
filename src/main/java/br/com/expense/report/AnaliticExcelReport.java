bbe2c25e0166fca62bb2ed4232d09f828f022ca4
Bjornn Borg <bjornnborg@gmail.com>
2013-10-14 20:53:43 -0300

    extracting methods in analitic report generator

--- src/main/java/br/com/expense/report/AnaliticExcelReport.java
+++ src/main/java/br/com/expense/report/AnaliticExcelReport.java
@@ -14,10 +14,6 @@ public class AnaliticExcelReport {
 	
 	private static final String SEPARATOR = ";";
 	private static final int BLANK_LINES = 2;
-	private static final int CATEGORY_FIELD_INDEX = 3;
-	private static final int AMOUNT_FIELD_INDEX = 2;
-	
-	private static final String[] FIELDS = {"A", "B", "C", "D"};
 	
 	private List<Transaction> transactions;
 	private CategoryExpenses expensesByCategory;
@@ -31,49 +27,15 @@ public class AnaliticExcelReport {
 	public String getContent() {
 		if (content == null) {
 			content = "";
-			int totalCategories = expensesByCategory.getCreditCount() + expensesByCategory.getDebitCount();
-			int totalTransactions = transactions.size();
-			Locale locale = Locale.getDefault();
-			int start = totalCategories + BLANK_LINES + 1;
-			int end = start + totalTransactions - 1;
+			FormulaHelper formulaHelper = new FormulaHelper(expensesByCategory);
 			for(Category category : expensesByCategory.allDebits()) {
 				content += category.getName() + SEPARATOR;
-				String formula = "";
-				if (new Locale("pt", "BR").equals(locale)) {
-					formula = "=SOMARPRODUTO";
-				} else {
-					formula = "=SUMPRODUCT";
-				}
-				
-				String textToSum = getTextToSum(category.getName());
-				
-				//=SUMPRODUCT((B6:B7<0)*(C6:C7="roupas"),B6:B7)
-				content += formula +
-						"(" +
-							"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + " < 0" + ") * " +
-							"(" + FIELDS[CATEGORY_FIELD_INDEX] + start + ":" + FIELDS[CATEGORY_FIELD_INDEX] + end + " = \"" + textToSum +"\") * " +
-							"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + ")" + 
-						")";
+				content += formulaHelper.getNegativeFormula(category);
 				content += "\r\n";
 			}
 			for(Category category : expensesByCategory.allCredits()) {
 				content += category.getName() + SEPARATOR;
-				String formula = "";
-				if (new Locale("pt", "BR").equals(locale)) {
-					formula = "=SOMARPRODUTO";
-				} else {
-					formula = "=SUMPRODUCT";
-				}
-				
-				String textToSum = getTextToSum(category.getName());
-				
-				//=SUMPRODUCT((B6:B7<0)*(C6:C7="roupas"),B6:B7)
-				content += formula +
-						"(" +
-							"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + " >= 0" + ") * " +
-							"(" + FIELDS[CATEGORY_FIELD_INDEX] + start + ":" + FIELDS[CATEGORY_FIELD_INDEX] + end + " = \"" + textToSum +"\") * " +
-							"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + ")" + 
-						")";
+				content += formulaHelper.getPositiveFormula(category);
 				content += "\r\n";
 			}			
 			
@@ -93,15 +55,57 @@ public class AnaliticExcelReport {
 		return content;
 	}
 
-	private String getTextToSum(String name) {
-		return "unspecified credit".equals(name) || "unspecified debit".equals(name) ? "" : name;
+	private static class FormulaHelper {
+		
+		private static final int CATEGORY_FIELD_INDEX = 3;
+		private static final int AMOUNT_FIELD_INDEX = 2;
+		private static final String[] FIELDS = {"A", "B", "C", "D"};
+		
+		private int start;
+		private int end;
+		
+		private FormulaHelper(CategoryExpenses expensesByCategory) {
+			int transactionsCount = expensesByCategory.allCredits().size() + expensesByCategory.allDebits().size(); 
+			int categoriesCount = expensesByCategory.getCreditCount() + expensesByCategory.getDebitCount();
+			start = categoriesCount + BLANK_LINES + 1;
+			end = start + transactionsCount - 1;			
+		}
+		
+		public String getPositiveFormula(Category category) {
+			return getLine(category, false);
+		}
+		
+		public String getNegativeFormula(Category category) {
+			return getLine(category, true);
+		}
+		
+		private String getLine(Category category, boolean negativeValue) {
+			String expressionLine = getFunction();
+			String value = " < 0";
+			if (!negativeValue) {
+				value = " >= 0";
+			}
+			expressionLine +=
+			"(" +
+				"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + value + ") * " +
+				"(" + FIELDS[CATEGORY_FIELD_INDEX] + start + ":" + FIELDS[CATEGORY_FIELD_INDEX] + end + " = \"" + getTextToSum(category.getName()) +"\") * " +
+				"(" + FIELDS[AMOUNT_FIELD_INDEX] + start + ":" + FIELDS[AMOUNT_FIELD_INDEX] + end + ")" + 
+			")";
+			return expressionLine;
+		}
+		
+		private String getFunction() {
+			Locale locale = Locale.getDefault();
+			String formula="=SUMPRODUCT";
+			if (new Locale("pt", "BR").equals(locale)) {
+				formula = "=SOMARPRODUTO";
+			}
+			return formula;
+		}
+		
+		private String getTextToSum(String name) {
+			return "unspecified credit".equals(name) || "unspecified debit".equals(name) ? "" : name;
+		}
 	}
 	
-	//=SUMPRODUCT(B5:B6,C5:C6="roupas",B5:B6>=0)
-	//{=SUM(($B$5:$B$6)*($B$5:$B$6<0)*($C$5:$C$6="roupas"))}
-	//=SUMPRODUCT((B6:B7<0)*(C6:C7="roupas"),B6:B7)
-	//SUM - SOMA
-	//SUMIF - SOMA.SE
-	//SUMPRODUCT - SOMARPRODUTO	
-
 }
